ad7df8c5b389b3aafa12c0dbaca58ffacefe5019
James Pannell <jance1499@gmail.com>
2020-04-30 02:50:33 -0500

    Added prodution deployment settings.

--- src/app/speedwalks/speedwalks.component.ts
+++ src/app/speedwalks/speedwalks.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit } from "@angular/core";
-import { ClipboardService } from "ngx-clipboard";
-import { faCopy } from "@fortawesome/free-regular-svg-icons";
-import { faFileCode } from "@fortawesome/free-regular-svg-icons";
-import { SpeedwalkService } from "./speedwalk.service";
-import { Speedwalk } from "./speedwalk";
+import { Component, OnInit } from '@angular/core';
+import { ClipboardService } from 'ngx-clipboard';
+import { faCopy } from '@fortawesome/free-regular-svg-icons';
+import { faFileCode } from '@fortawesome/free-regular-svg-icons';
+import { SpeedwalkService } from './speedwalk.service';
+import { Speedwalk } from './speedwalk';
 
 @Component({
-  selector: "app-speedwalks",
-  templateUrl: "./speedwalks.component.html",
-  styleUrls: ["./speedwalks.component.css"],
+  selector: 'app-speedwalks',
+  templateUrl: './speedwalks.component.html',
+  styleUrls: ['./speedwalks.component.css'],
 })
 export class SpeedwalksComponent implements OnInit {
   faCopy = faCopy;
   faFileCode = faFileCode;
   speedwalks: Speedwalk[];
-  delimiter = ";";
+  delimiter = ';';
 
   staticAlertClosed = true;
 
@@ -79,10 +79,10 @@ end`;
   }
 
   onCmudAliasCreationScript(): void {
-    let text = "";
+    let text = '';
     this.speedwalks.forEach((speedwalk) => {
       const directions = speedwalk.directions.join(this.delimiter);
-      text = text.concat(`#ALIAS ${speedwalk.name} {${directions}}`, "\n");
+      text = text.concat(`#ALIAS ${speedwalk.name} {${directions}}`, '\n');
     });
 
     this.clipboardService.copyFromContent(text);
@@ -91,15 +91,15 @@ end`;
   }
 
   onMudletAliasCreationScript(): void {
-    let text = "function createAlias()\n";
+    let text = 'function createAlias()\n';
     this.speedwalks.forEach((speedwalk) => {
       const directions = speedwalk.directions.join(this.delimiter);
       const createAliasScript = `if exists("${speedwalk.name}", "alias") == 0 then
   permAlias("${speedwalk.name}", "Speedwalks", "^${speedwalk.name}$", [[send ("${directions}")]])
 end`;
-      text = text.concat(createAliasScript, "\n");
+      text = text.concat(createAliasScript, '\n');
     });
-    text = text.concat("end");
+    text = text.concat('end');
 
     this.clipboardService.copyFromContent(text);
     this.staticAlertClosed = false;
@@ -112,6 +112,7 @@ end`;
 
   getSpeedwalks(): void {
     this.speedwalkService.getSpeedWalks().subscribe((speedwalks) => {
+      console.log(speedwalks);
       this.speedwalks = speedwalks as Speedwalk[];
     });
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDG1hCAx7egu52l6FgjeZlzX-w7KjXX5xI',
+    authDomain: 'uoss-app.firebaseapp.com',
+    databaseURL: 'https://uoss-app.firebaseio.com',
+    projectId: 'uoss-app',
+    storageBucket: 'uoss-app.appspot.com',
+    messagingSenderId: '781291371415',
+    appId: '1:781291371415:web:c2d94b47c7c3f7ab5522d2',
+    measurementId: 'G-47FCDK7LVX'
+  }
 };
