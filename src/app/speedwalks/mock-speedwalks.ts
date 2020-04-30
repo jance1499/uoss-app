e5e0613a3662a22bc3c5fe0b8e5609c7cae76b54
James Pannell <jance1499@gmail.com>
2020-04-30 11:57:05 -0500

    Removed id from mock speedwalks

--- src/app/speedwalks/mock-speedwalks.ts
+++ src/app/speedwalks/mock-speedwalks.ts
@@ -2,21 +2,18 @@ import { Speedwalk } from './speedwalk';
 
 export const SPEEDWALKS: Speedwalk[] = [
   {
-    id: 1,
     name: 'hidonwalk',
     directions: ['s', 's', 'ff3', 'say hidon'],
     category: 'Secret Hunt',
     description: 'Hidon'
   },
   {
-    id: 2,
     name: 'balcastlewalk',
     directions: ['e', 'e', 'ff5', 'airship', 'say bal castle'],
     category: 'Area',
     description: 'Bal Castle'
   },
   {
-    id: 3,
     name: 'mission1walk',
     directions: ['n', 'n', 'ct', 'nw', 'portal'],
     category: 'Mission',
